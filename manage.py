3ef0c4a677b62615befcfa3f70073293bfe2eccc
unknown <luizsilva21082004@gmail.com>
2024-03-18 19:58:58 -0300

    att bug editar perfil 2

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hashflix.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Wow.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
