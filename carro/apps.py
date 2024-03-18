cd45182ca22e3f3bc0ea9316adb5c6efae1ecdf6
unknown <luizsilva21082004@gmail.com>
2024-03-18 13:09:17 -0300

    criando superuser

--- carro/apps.py
+++ carro/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class CarroConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'carro'
+
+    def ready(self):
+        from .models import Usuario
+        import os
+
+        email = os.getenv('EMAIL_ADMIN')
+        senha = os.getenv('SENHA_ADMIN')
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            Usuario.objects.create_superuser(username="adm", email=email, password=senha,
+                                             is_active=True, is_staff=True)
