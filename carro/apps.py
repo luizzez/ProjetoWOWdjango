b2ab7358db7a018a2fa5493f84c495269087da32
unknown <luizsilva21082004@gmail.com>
2024-03-18 13:20:29 -0300

    superuser2

--- carro/apps.py
+++ carro/apps.py
@@ -9,10 +9,4 @@ class CarroConfig(AppConfig):
         from .models import Usuario
         import os
 
-        email = os.getenv('EMAIL_ADMIN')
-        senha = os.getenv('SENHA_ADMIN')
 
-        usuarios = Usuario.objects.filter(email=email)
-        if not usuarios:
-            Usuario.objects.create_superuser(username="adm", email=email, password=senha,
-                                             is_active=True, is_staff=True)
