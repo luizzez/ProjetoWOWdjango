ac1c30d1239e8addc64b97c1f6906f92731749ac
unknown <luizsilva21082004@gmail.com>
2024-03-18 19:15:06 -0300

    agora vai

--- manage.py
+++ manage.py
@@ -4,9 +4,10 @@ import os
 import sys
 from django.contrib.auth import get_user_model
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Wow.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hashflix.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,13 +18,14 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+    # Cria o superusuário se ele ainda não existir
+    User = get_user_model()
+    email = os.getenv("EMAIL_ADMIN")
+    senha = os.getenv("SENHA_ADMIN")
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin', email=email, password=senha, is_active=True, is_staff=True)
 
-User = get_user_model()
-email = os.getenv("EMAIL_ADMIN")
-senha = os.getenv("SENHA_ADMIN")
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser(
-        username='admin', email=email, password=senha, is_active=True, is_staff=True)
 
 if __name__ == '__main__':
     main()
