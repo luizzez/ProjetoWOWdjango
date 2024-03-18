adbbbed904cd3a4625d5ab4da4964cf593aaea8c
unknown <luizsilva21082004@gmail.com>
2024-03-18 13:51:03 -0300

    superuser5 nao aguento mais

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.contrib.auth import get_user_model
 
 def main():
     """Run administrative tasks."""
@@ -17,6 +17,13 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+
+User = get_user_model()
+email = os.getenv("EMAIL_ADMIN")
+senha = os.getenv("SENHA_ADMIN")
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin', email=email, password=senha, is_active=True, is_staff=True)
 
 if __name__ == '__main__':
     main()
