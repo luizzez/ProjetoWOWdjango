472536a316180ff6b98b15b6bba0e3a0f2af322b
unknown <luizsilva21082004@gmail.com>
2024-03-18 23:45:07 -0300

    commit 57

--- Wow/settings.py
+++ Wow/settings.py
@@ -149,14 +149,14 @@ STATIC_ROOT = BASE_DIR / 'static/'
 
 
 STATICFILES_DIRS = [
-    # BASE_DIR / "static"
+    BASE_DIR / "statics/static"
 ]
 
 
 MEDIA_URL = 'media/'
 
 
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
